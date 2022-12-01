848bcc44011c2267a60192a5f4dd07ae83bb88bc
Hedrekao <tomek.sloma321@gmail.com>
2022-12-01 14:05:59 +0100

    Fix to branch

--- Domain/Models/Ride.cs
+++ Domain/Models/Ride.cs
@@ -2,20 +2,6 @@
 
 public class Ride
 {
-<<<<<<< HEAD
-    public Location StartLocation { get; set; }
-    public Location Destination { get; set; }
-
-    public DateTime StartDate { get; set; }
-    public int Id { get; set; }
-    public string Driver { get; set; }
-
-    public Ride(Location startLocation, Location destination, DateTime startDate, int id, string driver)
-    {
-        StartLocation = startLocation;
-        Destination = destination;
-        StartDate = startDate;
-=======
     public int Id
     {
         get;
@@ -32,19 +18,14 @@ public class Ride
         Destination = destination;
         StartDate = startDate;
         StartLocation = startLocation;
->>>>>>> 9377c377bd76b5d36a5e260c7978fc3642f4f8b5
         Id = id;
         Driver = driver;
     }
 
     // public bool isBetweenDates(DateTime value)
     // {
-<<<<<<< HEAD
     //     return StartDate <= value.epoch && value.epoch <= EndDate.epoch;
     // }
     
-=======
-    //     return startDate.epoch <= value.epoch && value.epoch <= endDate.epoch;
-    // }
->>>>>>> 9377c377bd76b5d36a5e260c7978fc3642f4f8b5
+
 }
