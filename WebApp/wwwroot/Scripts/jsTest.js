cae1ec15508080d9fbf13d23131151819ea315ff
baginskijakub <92220319+baginskijakub@users.noreply.github.com>
2022-12-02 12:08:43 +0100

    Integration with google maps api
    -reverse geocoding not finished, result is there, we just need to integrate it with the component
    -map api finished

--- WebApp/wwwroot/Scripts/jsTest.js
+++ WebApp/wwwroot/Scripts/jsTest.js
@@ -1,23 +1,34 @@
-let map;
+var map;
 
 function consoleLog(text){
     console.log(text)
 }
  
-export function initialize() {
+function initialize() {
+    consoleLog("1")
     var latlng = new google.maps.LatLng(40.716948, -74.003563);
     var options = {
         zoom: 14, center: latlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+    consoleLog(document.getElementById ("map"))
+    const geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById ("map"), options);
 
     map.addListener("click", (mapsMouseEvent) => {
-
-        console.log(mapsMouseEvent.latLng)
+        let lat = mapsMouseEvent.latLng.lat()
+        let lng = mapsMouseEvent.latLng.lng()
+        DotNet.invokeMethodAsync("WebApp", "setCoordinates", lat, lng)
+        ConvertCoordinatesToAddress(geocoder, map, lat, lng)
     });
 }
 
-function getCoordinatesOnClick(){
+async function ConvertCoordinatesToAddress(geocoder, map, lat, lng){
+    
+    geocoder.geocode({
+        location: {lat : lat, lng : lng}}).then(res => {
+          console.log(res)
+            DotNet.invokeMethodAsync("WebApp", "setAddressLine", res.results[0].formatted_address)
+        })
 
 }
